0b4cdc5f676c6bc0d3d9bfc85b90b69f91f1b0b9
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-23 05:13:18 -0600

    Update IVEvent.cpp

--- src/IVEvent.cpp
+++ src/IVEvent.cpp
@@ -12,16 +12,19 @@ SettingEventType IVSettingEvent::getType() const noexcept {
 }
 
 IVSettingFilter::IVSettingFilter()
-    : m_type() {}
+    : m_type(std::nullopt) {}
 
 IVSettingFilter::IVSettingFilter(std::nullopt_t) : IVSettingFilter() {}
 
 IVSettingFilter::IVSettingFilter(SettingEventType type)
     : m_type(type) {}
 
-ListenerResult IVSettingFilter::handle(std::function<IVSettingFilter::Callback> fn, IVSettingEvent* event) {
+// CORRECCIÓN V5: Adiós std::function.
+ListenerResult IVSettingFilter::handle(geode::utils::MiniFunction<Callback> fn, IVSettingEvent* event) {
+    // Evaluamos si el tipo coincide
     if (m_type && m_type.value() != event->getType()) return ListenerResult::Propagate;
 
+    // Ejecutamos la función
     fn(event->getType());
     return ListenerResult::Propagate;
 }
