c2eecdc121133e1f1d33d5249bac6f6af2d048d6
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-23 05:40:30 -0600

    Update IVManager.hpp

--- src/class/IVManager.hpp
+++ src/class/IVManager.hpp
@@ -1,37 +1,10 @@
 #pragma once
-#include "IVLevelSettings.hpp"
+#include <Geode/Geode.hpp>
+#include <Geode/loader/Event.hpp>
 #include <Geode/loader/SettingV3.hpp>
 
 GEODE_NS_IV_BEGIN
 
-using RGBASettingFilter = geode::SettingChangedFilterV3;
-using RGBASettingListener = geode::EventListener<RGBASettingFilter>;
-
-class IVManager {
-public:
-    IVManager();
-    static IVManager& get();
-    static NodeTransform getDefaultP1Transform();
-    static NodeTransform getDefaultP2Transform();
-    LevelSettings& getLevelSettings(LevelSettingsType type) noexcept;
-public:
-    bool m_isInSetting;
-    cocos2d::ccColor4B m_backgroundPressColor;
-    cocos2d::ccColor4B m_backgroundReleaseColor;
-    cocos2d::ccColor4B m_outlinePressColor;
-    cocos2d::ccColor4B m_outlineReleaseColor;
-    cocos2d::ccColor4B m_textPressColor;
-    cocos2d::ccColor4B m_textReleaseColor;
-
-    LevelSettings m_settingClassic;
-    LevelSettings m_settingPlatformer;
-protected:
-    RGBASettingListener m_backgroundPressListener;
-    RGBASettingListener m_backgroundReleaseListener;
-    RGBASettingListener m_outlinePressListener;
-    RGBASettingListener m_outlineReleaseListener;
-    RGBASettingListener m_textPressListener;
-    RGBASettingListener m_textReleaseListener;
-};
-
-GEODE_NS_IV_END
+// Reemplazo seguro para los listeners de Settings V3
+using RGBASettingFilter = geode::SettingChangedEventV3;
+using RGBASettingListener = geode::EventListener<geode::SettingChangedEventV3>;
